94d460f0b81cee96f815adf5cd870eef2a3fc90b
nasunstar <rkdgms6900@gmail.com>
2025-11-11 20:06:38 +0900

    build: use foojay toolchain resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,15 +19,8 @@ dependencyResolutionManagement {
     }
 }
 
-toolchainManagement {
-    jvm {
-        jdkDownload {
-            repositories {
-                mavenCentral()
-                gradlePluginPortal()
-            }
-        }
-    }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "Agent_App"
